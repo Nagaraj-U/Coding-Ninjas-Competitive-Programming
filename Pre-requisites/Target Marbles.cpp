ab499fa4c251749af52590cc1956814c59f8bab2
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-08-10 23:10:10 +0530

    Update Target Marbles.cpp

--- Pre-requisites/Target Marbles.cpp
+++ Pre-requisites/Target Marbles.cpp
@@ -1,6 +1,8 @@
 /*
-At CodingNinjas, we love to play with marbles. We have many marble games, but the most popular one is “Target Marbles”. Now, our marbles are unique. Each marble has a number on it.
-In Target Marbles, the player is given a number in the starting and this number is called target. The player is also given N number of marbles to play with. Now, player has to arrange the marbles in a specific way such that sum of the values of at least one of the continuous subset of the arrangement is equal to given target.
+At CodingNinjas, we love to play with marbles. We have many marble games, but the most popular one is “Target Marbles”. Now, our marbles are unique. Each marble has a 
+number on it.
+In Target Marbles, the player is given a number in the starting and this number is called target. The player is also given N number of marbles to play with. Now,
+player has to arrange the marbles in a specific way such that sum of the values of at least one of the continuous subset of the arrangement is equal to given target.
 Now, NinjaCoder came to play this game and made an arrangement of marbles. The judges of the game need your help. You have to determine if NinjaCoder has won it or not.
 
 Input Format :
@@ -12,7 +14,8 @@ Constraints:
 Value on the marbles lies in the range [0, 1000].
 
 Output Format :
-You have to print “true”, if the NinjaCoder wins with the given arrangement and you have to print the values of the continuous subsets. If there are more that one continuous subsets, then you have to print the values of first continuous subset. If the Ninjas coder is unable to win, you just have to print “false”.
+You have to print “true”, if the NinjaCoder wins with the given arrangement and you have to print the values of the continuous subsets. If there are more that one continuous
+subsets, then you have to print the values of first continuous subset. If the Ninjas coder is unable to win, you just have to print “false”.
 
 Sample Input 1 :
 10 10
@@ -23,7 +26,8 @@ true
 9 1
 
 Explanation:
-Here, if the NinjaCoder arranges the given 10 marbles in this arrangement, then he/she will win the game. Now, there are many continuous subsets of marbles that will win the game such as (9,1) or (1, 2, 3, 4). Out of these winning combinations, you have to print the first one which is (9,1).
+Here, if the NinjaCoder arranges the given 10 marbles in this arrangement, then he/she will win the game. Now, there are many continuous subsets of marbles that will win the 
+game such as (9,1) or (1, 2, 3, 4). Out of these winning combinations, you have to print the first one which is (9,1).
 
 Sample Input 2 :
 10 10
