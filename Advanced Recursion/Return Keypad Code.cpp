5833f0ae315baaf47cb5761468fd5dbf73906ad4
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-08-25 22:00:41 +0530

    Update Return Keypad Code.cpp

--- Advanced Recursion/Return Keypad Code.cpp
+++ Advanced Recursion/Return Keypad Code.cpp
@@ -2,16 +2,21 @@
 
 Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
 Return empty string for numbers 0 and 1.
+
 Note : 1. The order of strings are not important.
-2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
+       2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
 Input Format :
 Integer n
+
 Output Format :
 All possible strings in different lines
+
 Constraints :
 1 <= n <= 10^6
+
 Sample Input:
 23
+
 Sample Output:
 ad
 ae
