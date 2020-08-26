2ee03737740743e12b6a6ff4f16d73f6ecb63560
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-08-26 22:27:20 +0530

    Update Sudoku Solver.cpp

--- Assignment - Backtracking , Binary Search And Merge Sort Problems/Sudoku Solver.cpp
+++ Assignment - Backtracking , Binary Search And Merge Sort Problems/Sudoku Solver.cpp
@@ -23,7 +23,80 @@ true
 
 #include<iostream>
 using namespace std;
-#include "Solution.h"
+
+bool isSafeInGrid(int board[][9],int row,int col,int num){
+    int rowFactor = row - (row%3);
+    int colFactor = col - (col%3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[i+rowFactor][j+colFactor] == num){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool isSafeInCol(int board[][9],int col,int num){
+    for(int i=0;i<9;i++){
+        if(board[i][col] == num){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isSafeInRow(int board[][9],int row,int num){
+    for(int i=0;i<9;i++){
+        if(board[row][i] == num){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isSafe(int board[][9],int row,int col,int num){
+    if(isSafeInRow(board,row,num) && isSafeInCol(board,col,num) && isSafeInGrid(board,row,col,num)){
+        return true;
+    }
+    return false;
+}
+
+bool findEmptyLocation(int board[][9],int &row,int &col){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(board[i][j] == 0){
+                row = i;
+                col = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool sudokuSolver(int board[][9]){
+
+  /* Don't write main().
+   *  Don't read input, it is passed as function argument.
+   *  Don't print output and return output as specified in the question
+  */
+   int row,col;
+   if(!findEmptyLocation(board,row,col)){
+       return true;
+   }
+    for(int i=1;i<=9;i++){
+        if(isSafe(board,row,col,i)){
+            board[row][col] = i;
+            if(sudokuSolver(board)){
+                return true;
+            }
+            board[row][col] = 0;
+        }
+    }
+    return false;
+}
+
 
 int main(){
 
