7a356c223c6dc1efb234f70b53d64f2562b1728f
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-08-30 00:11:45 +0530

    Update Print Keypad Combinations Code.cpp

--- Advanced Recursion/Print Keypad Combinations Code.cpp
+++ Advanced Recursion/Print Keypad Combinations Code.cpp
@@ -21,3 +21,48 @@ ce
 cf
 
 */
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <iostream>
+#include <string>
+using namespace std;
+string output[1000];
+const char key[10][5]={{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+
+void helper(int n,string output){
+  if(n==0){
+   
+    cout<<output<<endl;
+    return;
+  }
+  
+    int temp=n%10;
+	n=n/10;
+  	int j=-1;
+  	while(key[temp][++j]!='\0'){
+    helper(n,key[temp][j]+output); 
+    }
+  
+  
+}
+
+void printKeypad(int n){
+    /*
+    Given an integer number print all the possible combinations of the keypad. You do not need to return anything just print them.
+   */
+ helper(n,"");
+}
+
+int main(){
+    int num;
+    cin >> num;
+
+    printKeypad(num);
+
+    return 0;
+}
