bec040ca352a5cdc907bd8374a731e399aee6123
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-08-29 23:48:54 +0530

    Create sudoku.cpp

---
+++ Backtracking/sudoku.cpp
@@ -0,0 +1,34 @@
+/*
+Sudoku
+Write a program to find a solution to a sudoku puzzle. The input will consist of puzzles with exactly one solution. Or in other words, the Sudoku solved must be a legitimate Sudoku!
+
+Input
+The input consists of one test case. Each test case will consist of nine lines with nine digits on each line. There will not be any blanks between the digits. The digits '1' through '9' will represent a number and the digit '0' will represent a blank square. The end of input is indicated by the end of the file.
+
+Output
+For each test case print, in the format shown below, the solved puzzle. There should be no spaces between digits and lines.
+
+Example
+Input:
+023456789
+406789123
+780123456
+234067891
+567801234
+891230567
+345678012
+678912305
+912345670
+
+
+Output:
+123456789
+456789123
+789123456
+234567891
+567891234
+891234567
+345678912
+678912345
+912345678
+*/
