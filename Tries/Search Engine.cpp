247a2f221b40202c04ca4c51ad915268f12d51ea
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-09-28 22:41:14 +0530

    Update Search Engine.cpp

--- Tries/Search Engine.cpp
+++ Tries/Search Engine.cpp
@@ -1,6 +1,6 @@
 /*
 Search Engine
-Send Feedback
+
 Let us see how search engines work. Consider the following simple auto complete feature. When you type some characters in the text bar, the engine automatically gives best matching options among it's database. Your job is simple. Given an incomplete search text, output the best search result.
 Each entry in engine's database has a priority factor attached to it. We consider a result / search suggestion best if it has maximum weight and completes the given incomplete search query. For each query in the input, print the maximum weight of the string in the database, that completes the given incomplete search string. In case no such string exists, print -1.
 INPUT
