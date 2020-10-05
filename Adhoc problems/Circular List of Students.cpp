5d687f49757452bf1e20355a23cc7ed909b8b29f
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-10-05 22:35:59 +0530

    Update Circular List of Students.cpp

--- Adhoc problems/Circular List of Students.cpp
+++ Adhoc problems/Circular List of Students.cpp
@@ -1,4 +1,5 @@
 /*
+
 You are given a circular list of students as follows:
 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 11
 This list is circular, means that 11 will follow 0 again. You will be given the student number ‘i’ and some position ‘p’. You will have to tell that if the list will start 
