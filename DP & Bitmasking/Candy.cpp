0c144de486af24bbdb3d05aee279f42390227ba3
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-10-06 22:50:47 +0530

    Update Candy.cpp

--- DP & Bitmasking/Candy.cpp
+++ DP & Bitmasking/Candy.cpp
@@ -1,7 +1,9 @@
 /*
 Candy
 Send Feedback
-Gary is a teacher at XYZ school. To reward his N students he bought a packet of N candies all with different flavours. But the problem is some students like certain flavour while some doesn't. Now Gary wants to know the number of ways he can distribute these N candies to his N students such that every student gets exactly one candy he likes.
+Gary is a teacher at XYZ school. To reward his N students he bought a packet of N candies all with different flavours. But the problem is some students 
+like certain flavour while some doesn't. Now Gary wants to know the number of ways he can distribute these N candies to his N students such that every student gets
+exactly one candy he likes.
 Input Format :
 Line 1 : An integer N (1<= N <= 16) denoting number of students and candies.
 Next N lines : N integers describing the preferences of one student. 1 at i'th (0 <= i < N) position denotes that this student likes i'th candy , 0 means he doesn't.
