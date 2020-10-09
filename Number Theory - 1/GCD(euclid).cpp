0547a0c7997476b56d29dba48ebf4ac5c4fd99eb
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-10-09 16:55:35 +0530

    Update GCD(euclid).cpp

--- Number Theory - 1/GCD(euclid).cpp
+++ Number Theory - 1/GCD(euclid).cpp
@@ -17,3 +17,28 @@ Sample Input 2:
 Sample Output 2:
 2
 */
+// void swap(int &a,int &b){
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     return;
+// }
+
+
+
+//time complexity of algorithm is O(log2(max(a,b)))
+int gcd(int a,int b)
+{
+	
+    // if(b>a){
+    //     swap(a,b);
+    // }
+    
+    //no need to handle b>a case algorithm handle itself (dry run to confirm)
+    if(b == 0){
+        return a;
+    }
+    
+    return gcd(b,a%b);
+}
+
