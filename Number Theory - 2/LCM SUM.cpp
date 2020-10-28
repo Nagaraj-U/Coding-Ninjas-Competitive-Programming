ca2110e5851d6e8d5acf6d9134024a2d5a7703a4
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-10-28 23:09:55 +0530

    Create LCM SUM.cpp

---
+++ Number Theory - 2/LCM SUM.cpp
@@ -0,0 +1,22 @@
+/*
+LCM SUM
+Send Feedback
+Given n, calculate and print the sum :
+LCM(1,n) + LCM(2,n) + .. + LCM(n,n)
+where LCM(i,n) denotes the Least Common Multiple of the integers i and n.
+Input Format :
+Line 1 : Integer n
+Output Format :
+Required sum
+Constraints :
+1 <= T <= 300000
+1 <= n <= 1000000
+Sample Input 1 :
+5
+Sample Output 1 :
+55
+Sample Input 2 :
+2
+Sample Output 2 :
+4
+*/
