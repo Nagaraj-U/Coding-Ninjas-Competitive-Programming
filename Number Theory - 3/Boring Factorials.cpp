a589145a362d0817280bea4325c7658f19be59b3
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-11-01 22:01:16 +0530

    Update Boring Factorials.cpp

--- Number Theory - 3/Boring Factorials.cpp
+++ Number Theory - 3/Boring Factorials.cpp
@@ -26,3 +26,69 @@ Sample Output:
 10
 6
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+/*
+wilsons theorom:
+(p-1)! mod p = -1 or (p-1)  given p is prime
+
+fermats little theorom::
+(1/a)%p = (a^(p-2))%p  where 'p' is prime and 'a' is integer (here 1/a = a inverse)
+ 
+ 
+application : find (n!)%p
+
+1. for n>p  (n!)%p = 0 since 'p' is one of factor
+
+2. for n<p  (n!)%p can be written as
+
+((1*2*3...n)(n+1*n+2*....p-1))%p = -1 (wilson theorom)
+(n!)%p = -1 * (n+1)^-1 % p * (n+2)^-1%p * .... * (p-1)^-1 % p   note : (here (n+1)^-1 is (n+1) inverse)
+
+now applying fermats little theorom (1/a)%p = (a^(p-2))%p  where 'p' is prime and 'a' is integer (here 1/a = a inverse)
+*/
+
+
+
+// overall time complexity is O(1000*1000*log(p))
+ll power(ll num,ll p,ll mod){ //this takes log p  time
+    if(p == 0){
+        return 1;
+    }
+    if(p == 1){
+        return num;
+    }
+    ll ans = power(num,p/2,mod)%mod;
+    ans = ((ans) * (ans))%mod;
+    if(p%2 != 0){
+        ans = (ans%mod * num%mod)%mod;
+    }
+    
+    return ans;
+}
+
+int main() {
+
+	// Write your code here
+    int t;
+    cin >> t;
+    while(t--){ //this can be max 1000
+        ll n,p;
+        cin >> n >> p;
+        if(n>=p){
+            cout << 0 << endl;
+            continue;
+        }else{
+            ll ans = -1;
+            for(ll i=(n+1);i<=(p-1);i++){ //this is max = 1000 itertaion
+                ll res = power(i,p-2,p); 
+                res = res%p;
+                ans = ((ans%p) * (res%p))%p;
+                ans = (ans+p)%p;
+            }
+            cout << ans << endl;
+        }
+    }
+}
