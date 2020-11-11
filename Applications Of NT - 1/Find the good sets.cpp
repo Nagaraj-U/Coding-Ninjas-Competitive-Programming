b558c4a1c98905cd66e7f7655492d1539668dfa7
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-11-11 21:54:41 +0530

    Update Find the good sets.cpp

--- Applications Of NT - 1/Find the good sets.cpp
+++ Applications Of NT - 1/Find the good sets.cpp
@@ -1,4 +1,5 @@
 /*
+
 Find the good sets!
 Send Feedback
 You are given array a consisting of n distinct integers. A set s of numbers is called good if you can rearrange the elements in such a way that each element divides the next element in the order, i.e. 'si' divides 'si + 1', such that i < |s|, where |s| denotes size of set |s|.
