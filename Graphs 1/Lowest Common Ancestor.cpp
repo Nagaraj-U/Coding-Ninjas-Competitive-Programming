6b3887480b073a8a668d7cede2534a2e840f9def
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-04-05 19:50:14 +0530

    Update and rename Longest Common Ancestor.cpp to Lowest Common Ancestor.cpp

--- Graphs 1/Lowest Common Ancestor.cpp
+++ Graphs 1/Lowest Common Ancestor.cpp
@@ -98,6 +98,34 @@ For the second test case. the LCA of 5,6,7 in the given tree is 1,
 */
 
 
+/*
+APPROACH
+
+
+By Storing Paths
+A simple solution would be to store the path from the root to N1, the path from the root to N2, and the path from the root to N3 in the three auxiliary Lists/Array.
+Now, to store the path from the root to any node X we create a recursive function that traverses the different path in the binary tree to find any node X:
+If root == null return true
+Add the root data to List/Array
+If root data = X return true
+If X is found in any of the subtrees either left or right then return true
+Else remove the current root data from the List/Array and then return false
+Then we traverse all lists simultaneously till the values in the list match. The last matched value will be the LCA. If the end of one array is reached then the last seen value is LCA.
+Time Complexity
+O(N), where N is the total number of nodes in the given binary tree.
+
+In the worst case, the tree is traversed thrice to find paths of N1, N2, and N3 O(N), and then the stored path list O(N) is traversed. Thus a total of O(N).
+
+Space Complexity
+O(N), where N is the total number of nodes in the given binary tree.
+
+In the worst case, we will store all the nodes in the list as a path from the root to the node. And we will have all the nodes of the Binary Tree in the recursion stack O(N). Thus a total of O(N).
+
+
+
+*/
+
+
 
 /**********************************************************
     Following is the Binary Tree Node class structure
@@ -117,6 +145,9 @@ For the second test case. the LCA of 5,6,7 in the given tree is 1,
     };
 
 ***********************************************************/
+
+
+
 
 bool helper(int node,BinaryTreeNode<int>* root, vector<BinaryTreeNode<int>* > &path)
 {
