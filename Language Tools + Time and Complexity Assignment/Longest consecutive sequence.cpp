81e4f064f7c5a8b403f69a30997acfa20f766ada
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-12 11:42:37 +0530

    optimized

--- Language Tools + Time and Complexity Assignment/Longest consecutive sequence.cpp
+++ Language Tools + Time and Complexity Assignment/Longest consecutive sequence.cpp
@@ -35,82 +35,56 @@ Sample Output 3 :
 16
 */
 
-#include<iostream>
-#include<map>
-#include<utility>
-#include<vector>
-using namespace std;
 
-vector<int> longestConsecutiveIncreasingSequence(int *arr, int n){
-	//Your Code goes here
-    vector<pair<int, int> >pairs_vector;
-	//in this vector first element of each pair is the start and second element is the max_length.
-	map<int, bool>m;
-	for (int i = 0; i < n; i++)
-	{
-		m[arr[i]] = true;
-	}
-	int max_length = 0;
-	int start = 0;
-	for (int i = 0; i < n; i++)
-	{
-		int current_length = 1;
-		int current_start = 0;
-		if (m[arr[i]] = true)
-		{
-			m[arr[i]] = false;
-			//forward propagation from arr[i]
-			int element_at_position = arr[i];
-			while (m[element_at_position + 1] == true)
-			{
-				current_length += 1;
-				m[element_at_position + 1] = false;
-				element_at_position++;
-			}
-			//backward propagation from arr[i]
-			element_at_position = arr[i];
-			current_start = element_at_position;
-			while (m[element_at_position - 1] == true)
-			{
-				m[element_at_position - 1] = false;
-				current_start--;
-				current_length++;
-				element_at_position--;
-			}
-		}
-		if (current_length >= max_length)
-		{
-			max_length = current_length;
-			start = current_start;
-			pair<int, int>p;
-			p.first = start;
-			p.second = max_length;
-			pairs_vector.push_back(p);
-			//in this vector first element of each pair is the START and second element is the MAX_LENGTH.
-		}
-	}
-	if (max_length == 1 && start == arr[n - 1])
-	{
-		vector<int>temp;
-		temp.push_back(arr[0]);
-		return temp;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		int starting_element = arr[i];
-		for (int j = 0; j < pairs_vector.size(); j++)
-		{
-			if (starting_element == pairs_vector[j].first && max_length == pairs_vector[j].second)
-			{
-				vector<int>temp;
-				for (int k = starting_element; k < starting_element + max_length; k++)
-				{
-					temp.push_back(k);
-				}
-				return temp;
-			}
-		}
-	}
+
+//O(nlog n)
+
+#include<bits/stdc++.h>
+vector<int> longestConsecutiveIncreasingSequence(int *arr, int n) {
+    // Your Code goes here
+    int* dp = new int[n]();
+    unordered_map<int,int> m;
+    int index = 0;
+    for(int i=0;i<n;i++){
+        m[arr[i]] = index++; //storing rank for first occurance
+    }
+    m[-1] = n+1;
+    for(int i=0;i<n;i++){
+        dp[i] = 1;
+    }
+    sort(arr,arr+n);
+    for(int i=1;i<n;i++){
+        if(arr[i-1]+1 == arr[i]){
+            dp[i] = dp[i-1] + 1; //if prev element is consecutive
+        }
+    }
+    int maxi = INT_MIN; //max length of subsequence possible
+    for(int i=0;i<n;i++){
+        maxi = max(maxi,dp[i]);
+    }
+
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        if(dp[i] == maxi){ //all possible subsequences of length "maximum"
+            ans.push_back(arr[i]-dp[i]+1);
+        }
+    }
+
+    int idx = n+1;
+    int finalAns; //among all possible answers onw which has first occurance is original array
+	for(int i=0;i<ans.size();i++){
+        if(m[ans[i]]<idx){
+            idx = m[ans[i]];
+            finalAns = ans[i]; 
+        }
+    }
+
+    vector<int> v;
+    v.push_back(finalAns);
+    v.push_back(finalAns+maxi-1);
+    
+    return v;
+    
 }
 
 
@@ -125,53 +99,101 @@ vector<int> longestConsecutiveIncreasingSequence(int *arr, int n){
 
 
 
-// #include <vector>
+
+
+
+
+
+
+
+
+// #include<iostream>
 // #include<map>
-// #include<algorithm>
-// #include<bits/stdc++.h>
-// #define min -9999
-// #define max 9999
+// #include<utility>
+// #include<vector>
 // using namespace std;
 
 // vector<int> longestConsecutiveIncreasingSequence(int *arr, int n){
 // 	//Your Code goes here
-//     map<int,int> m;
-//     vector<int> v;
-    
-//     for(int i=0;i<n;i++){
-//         m[arr[i]] = i+1;
-//     }
-//     int count = 1;
-//     int start = min;
-//     int best_start = arr[0];
-//     int best_count = min;
-//     sort(arr,arr+n);
-//     for(int i=1;i<n;i++){
-//         if(arr[i-1]+1 == arr[i]){
-//             if(start == min){
-//                 start = arr[i-1];
-//             }
-//             count++;
-//         }else{
-//             if(count > best_count){
-//                 best_start = start;
-//                 best_count = count;
-//             }else if(count == best_count){
-//                 if(m[start] < m[best_start]){
-//                     best_start = start;
-//                     best_count = count;
-//                 }
-//             }
-//             start = min;
-//             count = 1;
-//         }    
-//     }
-//     for(int i=0;i<count;i++){
-//         v.push_back(best_start);
-//         best_start++;
-//     }
-   
-//     return v;
-    
+//     vector<pair<int, int> >pairs_vector;
+// 	//in this vector first element of each pair is the start and second element is the max_length.
+// 	map<int, bool>m;
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		m[arr[i]] = true;
+// 	}
+// 	int max_length = 0;
+// 	int start = 0;
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		int current_length = 1;
+// 		int current_start = 0;
+// 		if (m[arr[i]] = true)
+// 		{
+// 			m[arr[i]] = false;
+// 			//forward propagation from arr[i]
+// 			int element_at_position = arr[i];
+// 			while (m[element_at_position + 1] == true)
+// 			{
+// 				current_length += 1;
+// 				m[element_at_position + 1] = false;
+// 				element_at_position++;
+// 			}
+// 			//backward propagation from arr[i]
+// 			element_at_position = arr[i];
+// 			current_start = element_at_position;
+// 			while (m[element_at_position - 1] == true)
+// 			{
+// 				m[element_at_position - 1] = false;
+// 				current_start--;
+// 				current_length++;
+// 				element_at_position--;
+// 			}
+// 		}
+// 		if (current_length >= max_length)
+// 		{
+// 			max_length = current_length;
+// 			start = current_start;
+// 			pair<int, int>p;
+// 			p.first = start;
+// 			p.second = max_length;
+// 			pairs_vector.push_back(p);
+// 			//in this vector first element of each pair is the START and second element is the MAX_LENGTH.
+// 		}
+// 	}
+// 	if (max_length == 1 && start == arr[n - 1])
+// 	{
+// 		vector<int>temp;
+// 		temp.push_back(arr[0]);
+// 		return temp;
+// 	}
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		int starting_element = arr[i];
+// 		for (int j = 0; j < pairs_vector.size(); j++)
+// 		{
+// 			if (starting_element == pairs_vector[j].first && max_length == pairs_vector[j].second)
+// 			{
+// 				vector<int>temp;
+// 				for (int k = starting_element; k < starting_element + max_length; k++)
+// 				{
+// 					temp.push_back(k);
+// 				}
+// 				return temp;
+// 			}
+// 		}
+// 	}
 // }
+
+
+
+
+
+
+
+
+
+
+
+
 
