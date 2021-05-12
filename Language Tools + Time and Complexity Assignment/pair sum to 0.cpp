13e4d4724a4a84ae13a776ded259186fdd249c95
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-12 10:23:08 +0530

    optimized

--- Language Tools + Time and Complexity Assignment/pair sum to 0.cpp
+++ Language Tools + Time and Complexity Assignment/pair sum to 0.cpp
@@ -23,6 +23,33 @@ Sample Output :
 */
 
 
+#include<bits/stdc++.h>
+int pairSum(int *arr, int n) {
+
+    unordered_map<int,int> m;
+    for(int i=0;i<n;i++){
+        m[arr[i]]++;
+    }
+    int count = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i] == 0){//handling zeros separately
+            continue;
+        }
+        int comp = -arr[i];
+        if(m[comp] != 0){
+            count += (m[arr[i]] * m[comp]);
+            m[comp] = 0;
+            m[arr[i]] = 0;
+        }
+    }
+    if(m[0] != 0){ // for 0000 = total = 6
+        int temp = m[0]-1;
+        count += (temp*(temp+1)/2);
+    }
+    return count;
+}
+
+/*
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -62,3 +89,4 @@ int main()
 
 	return 0;
 }
+*/
