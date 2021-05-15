2a5e2cb2feaa15ff140a9c6afccdd0a237438097
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-15 19:55:12 +0530

    Update Fractional Knapsack.cpp

--- Greedy Problems/Fractional Knapsack.cpp
+++ Greedy Problems/Fractional Knapsack.cpp
@@ -1,11 +1,17 @@
 /*
 Fractional Knapsack
+
 Send Feedback
-You want to paint your house. The total area of your house is D units. There are a total of N workers. The ith worker is available after time Ti, has hiring cost Xi and speed Yi. This means he becomes available for hiring from time Ti and remains available after that. Once available, you can hire him with cost Xi, after which he will start painting the house immediately, covering exactly Yi units of house with paint per time unit. You may or may not hire a worker and can also hire or fire him at any later point of time. However, no more than 1 worker can be painting the house at a given time.
-Since you want the work to be done as fast as possible, figure out a way to hire the workers, such that your house gets painted at the earliest possible time, with minimum cost to spend for hiring workers.
+You want to paint your house. The total area of your house is D units. There are a total of N workers. The ith worker is available after time Ti, has hiring cost Xi and 
+speed Yi. This means he becomes available for hiring from time Ti and remains available after that. Once available, you can hire him with cost Xi, after which he will start
+painting the house immediately, covering exactly Yi units of house with paint per time unit. You may or may not hire a worker and can also hire or fire him at any later point
+of time. However, no more than 1 worker can be painting the house at a given time.
+Since you want the work to be done as fast as possible, figure out a way to hire the workers, such that your house gets painted at the earliest possible time, with minimum cost
+to spend for hiring workers.
 Note: You can hire a previously hired worker without paying him again.
 Input
-The first line of input contains two integers "N D", the number of workers and the area of your house respectively. The ith of the next N lines denotes the ith worker, and contains three integers "Ti Xi Yi", described in the statement.
+The first line of input contains two integers "N D", the number of workers and the area of your house respectively. The ith of the next N lines denotes the ith worker,
+and contains three integers "Ti Xi Yi", described in the statement.
 Output
 Output one integer, the minimum cost that you can spend in order to get your house painted at the earliest.
 Constraints
