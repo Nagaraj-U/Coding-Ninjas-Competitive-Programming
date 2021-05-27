43977aa34da6cd4254c826a1658f73a98e806623
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-27 23:49:36 +0530

    Create Floyd Warshall Algorithm .cpp

---
+++ Graphs 2/Floyd Warshall Algorithm .cpp
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+//All pairs shortest path
+//works for all types of graph directed,undirected
+//idea is consider all passobility of edges between (i,j) and try to to add vertex " k" between them 
+//if adding "k" reduces the shortest path update it 
+//relaxing every edge "k" times
+
+//Put some large values if no edge is present between two vertices in adjacency matrix
+
+void warshall(int** arr,int n)
+{
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                arr[i][j] = min(arr[i][j],arr[i][k]+arr[k][j]);
+            }
+        }
+    }
+    return;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int** arr = new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i] = new int[n];
+    }
+    
+    for(int  i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> arr[i][j];        
+        }
+    }
+    
+    warshall(arr,n);
+    
+    for(int  i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout << arr[i][j] << " ";        
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
