948f15b7d8961d26298075d8de017ba44c7bb9c1
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-31 15:48:16 +0530

    Create Dp : Longest Common Subsequence.cpp

---
+++ Commonly Asked Problems Practice/Dp : Longest Common Subsequence.cpp
@@ -0,0 +1,72 @@
+/*
+Longest Common Subsequence
+Medium
+
+3145
+
+39
+
+Add to List
+
+Share
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the 
+remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+
+Constraints:
+
+1 <= text1.length, text2.length <= 1000
+text1 and text2 consist of only lowercase English characters.
+*/
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int** dp = new int*[m+1];
+        for(int j=0;j<=m;j++){
+            dp[j] = new int[n+1];
+            for(int i=0;i<=n;i++){
+                dp[j][i] = 0;
+            }
+        }
+        
+        //each cell denotes longest common subsequence of s1 and s2 of lengths "i" and "j" respectively
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
