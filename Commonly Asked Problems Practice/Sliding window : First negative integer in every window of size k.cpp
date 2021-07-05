4d35e2c8d47b2e043e01cf29bda084598d63bcab
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-07-05 20:06:49 +0530

    Create Sliding window : First negative integer in every window of size k.cpp

---
+++ Commonly Asked Problems Practice/Sliding window : First negative integer in every window of size k.cpp
@@ -0,0 +1,75 @@
+/*
+Given an array A[] of size N and a positive integer K, find the first negative integer for each and every window(contiguous subarray) of size K.
+
+ 
+
+Example 1:
+
+Input : 
+N = 5
+A[] = {-8, 2, 3, -6, 10}
+K = 2
+Output : 
+-8 0 -6 -6
+Explanation :
+First negative integer for each window of size k
+{-8, 2} = -8
+{2, 3} = 0 (does not contain a negative integer)
+{3, -6} = -6
+{-6, 10} = -6
+ 
+Example 2:
+Input : 
+N = 8
+A[] = {12, -1, -7, 8, -15, 30, 16, 28}
+K = 3
+Output :
+-1 -1 -7 -15 -15 0 
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function printFirstNegativeInteger() which takes the array A[], 
+its size N and an integer K as inputs and returns the first negative number in every window of size K starting from the first till the end.
+If a window does not contain a negative integer , then return 0 for that window.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(K)
+
+Constraints:
+1 <= N <= 105
+1 <= A[i] <= 105
+1 <= K <= N
+*/
+
+
+typedef long long ll;
+
+vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K){
+    
+    queue<int> q; //to store index of all negative elements
+    int start = 0;
+    int end = 0;
+    vector<ll> ans;
+    while(end < N){
+        if(A[end] < 0){
+            q.push(end);
+        }
+        if(end-start+1 == K){
+            while(!q.empty() and q.front() < start){ //remove all negative elements which are out of window
+                q.pop();
+            }
+            
+            if(q.empty()){ //for this window no -ve element present
+                ans.push_back(0);
+            }else{
+                ans.push_back(A[q.front()]); //first element in in queue
+            }
+            
+            start++;
+        }
+        
+        end++;
+    }
+    return ans;
+}
