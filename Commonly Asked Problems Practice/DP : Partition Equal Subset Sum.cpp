5135955a72f01420a4a6b6dc40e55d9e4ee55d7e
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-08-06 00:49:12 +0530

    Update DP : Partition Equal Subset Sum.cpp

--- Commonly Asked Problems Practice/DP : Partition Equal Subset Sum.cpp
+++ Commonly Asked Problems Practice/DP : Partition Equal Subset Sum.cpp
@@ -1,5 +1,6 @@
 /*
-Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both 
+subsets is equal.
 
  
 
