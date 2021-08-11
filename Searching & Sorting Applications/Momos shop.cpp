91f3d3c21237008cba7d534508858c49730ec818
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-08-11 11:15:21 +0530

    Update Momos shop.cpp

--- Searching & Sorting Applications/Momos shop.cpp
+++ Searching & Sorting Applications/Momos shop.cpp
@@ -1,8 +1,11 @@
 /*
-Shreya loves to eat momos. Her mother gives her money to buy vegetables but she manages to save some money out of it daily. After buying vegetables, she goes to "Momos Market",
-where there are ‘n’ number of shops of momos. Each of the shops of momos has a rate per momo. She visits the market and starts buying momos (one from each shop) starting
+Shreya loves to eat momos. Her mother gives her money to buy vegetables but she manages to save some money out of it daily. After buying vegetables,
+she goes to "Momos Market",
+where there are ‘n’ number of shops of momos. Each of the shops of momos has a rate per momo. She visits the market and starts buying momos (one from each shop) 
+starting
 from the first shop. She will visit the market for ‘q’ days. You have to tell that how many momos she can buy each day if she starts buying from the first shop daily. 
-She cannot use the remaining money of one day on some other day. But she will save them for other expenses in the future, so, you also need to tell the sum of money left 
+She cannot use the remaining money of one day on some other day. But she will save them for other expenses in the future, so, you also need to tell the sum of 
+money left 
 with her at the end of each day.
 Input Format:
 First line will have an integer ‘n’ denoting the number of shops in market.
@@ -23,7 +26,8 @@ Sample Input:
 Sample Output:
 3 2
 Explanation:
-Shreya visits the "Momos Market" for only one day. She has 11 INR to spend. She can buy 3 momos, each from the first 3 shops. She would 9 INR (2 + 1 + 6) for the same and hence, she will save 2 INR.
+Shreya visits the "Momos Market" for only one day. She has 11 INR to spend. She can buy 3 momos, each from the first 3 shops. She would 9 INR (2 + 1 + 6) 
+for the same and hence, she will save 2 INR.
 */
 
 
