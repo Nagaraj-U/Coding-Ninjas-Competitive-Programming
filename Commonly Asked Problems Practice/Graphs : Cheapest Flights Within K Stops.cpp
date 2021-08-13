4a6954f49917830a7f764e069c854f99080fb057
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-08-14 00:14:06 +0530

    Update Graphs : Cheapest Flights Within K Stops.cpp

--- Commonly Asked Problems Practice/Graphs : Cheapest Flights Within K Stops.cpp
+++ Commonly Asked Problems Practice/Graphs : Cheapest Flights Within K Stops.cpp
@@ -1,5 +1,6 @@
 /*
-There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight 
+from city fromi to city toi with cost pricei.
 
 You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
 
