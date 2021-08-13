12abc8f3f86e21add3c9d9f9cb665f527f4ceac4
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-08-13 23:40:01 +0530

    Update Graphs : Alien Dictionary ( Topo sort ).cpp

--- Commonly Asked Problems Practice/Graphs : Alien Dictionary ( Topo sort ).cpp
+++ Commonly Asked Problems Practice/Graphs : Alien Dictionary ( Topo sort ).cpp
@@ -1,7 +1,7 @@
 /*
 Given a sorted dictionary of an alien language having N words and k starting alphabets of standard dictionary. Find the order of characters in the alien language.
-Note: Many orders may be possible for a particular test case, thus you may return any valid order and output will be 1 if the order of string returned by the function is 
-correct else 0 denoting incorrect string returned.
+Note: Many orders may be possible for a particular test case, thus you may return any valid order and output will be 1 if the order of string returned by the function 
+is correct else 0 denoting incorrect string returned.
  
 
 Example 1:
