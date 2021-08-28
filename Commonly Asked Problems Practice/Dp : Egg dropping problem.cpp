91d75924f74850587d289dc88f3a438f234ba8f2
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-08-28 23:09:28 +0530

    Update Dp : Egg dropping problem.cpp

--- Commonly Asked Problems Practice/Dp : Egg dropping problem.cpp
+++ Commonly Asked Problems Practice/Dp : Egg dropping problem.cpp
@@ -1,6 +1,7 @@
 /*
 You are given k identical eggs and you have access to a building with n floors labeled from 1 to n.
-You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below floor f will not break.
+You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or 
+below floor f will not break.
 Each move, you may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If the egg breaks, you can no longer use it. However,
 if the egg does not break, you may reuse it in future moves.
 Return the minimum number of moves that you need to determine with certainty what the value of f is.
