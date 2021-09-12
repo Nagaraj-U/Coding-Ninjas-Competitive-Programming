431c158a62fff3eb302662b7823edd7694afc061
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-09-12 13:26:00 +0530

    Update Count number of binary strings without consecutive 1’s.cpp

--- Commonly Asked Problems Practice/Count number of binary strings without consecutive 1’s.cpp
+++ Commonly Asked Problems Practice/Count number of binary strings without consecutive 1’s.cpp
@@ -1,3 +1,25 @@
+
+/*
+Given a positive integer N, count all possible distinct binary strings of length N such that there are no consecutive 1’s.
+Eg
+
+Input:  N = 2
+Output: 3
+// The 3 strings are 00, 01, 10
+
+Input: N = 3
+Output: 5
+// The 5 strings are 000, 001, 010, 100, 101
+
+
+APPROACH : 
+Let a[i] be the number of binary strings of length i which do not contain any two consecutive 1’s and which end in 0.
+Similarly, let b[i] be the number of such strings which end in 1.
+
+Note : We can append either 0 or 1 to a string ending in 0, 
+but we can only append 0 to a string ending in 1.
+
+*/
 #include <iostream>
 using namespace std;
 
