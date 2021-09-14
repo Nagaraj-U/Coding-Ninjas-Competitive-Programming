568a62eae50e55f1baf971f7d6a0a701d34c92d4
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-09-14 12:52:50 +0530

    Update Trees : Convert a given tree to its Sum Tree.cpp

--- Commonly Asked Problems Practice/Trees : Convert a given tree to its Sum Tree.cpp
+++ Commonly Asked Problems Practice/Trees : Convert a given tree to its Sum Tree.cpp
@@ -4,68 +4,68 @@ right sub trees in the original tree.
 */
 
 
-#include<bits/stdc++.h>
-using namespace std;
+/*
+Given a Binary Tree of size N , where each node can have positive or negative values. Convert this to a tree where each node 
+contains the sum of the left and right sub trees of the original tree. The values of leaf nodes are changed to 0.
 
-class node
-{
-public:
-    int data;
-    node* left;
-    node* right;
-    node(int val){
-        this->data = val;
-        left = NULL;
-        right = NULL;
-    }
+Example 1:
+
+Input:
+             10
+          /      \
+        -2        6
+       /   \     /  \
+     8     -4   7    5
+
+Output:
+            20
+          /    \
+        4        12
+       /  \     /  \
+     0     0   0    0
+
+Explanation:
+
+           (4-2+12+6)
+          /           \
+      (8-4)          (7+5)
+       /   \         /  \
+      0     0       0    0
+ 
+
+Your Task:  
+You dont need to read input or print anything. Complete the function toSumTree() which takes root node as input parameter and modifies the given tree in-place.
+
+Note: If you click on Compile and Test the output will be the in-order traversal of the modified tree.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(height of tree)
+*/
+class Solution {
+  public:
+  
+    // Convert a given tree to a tree where every node contains sum of values of
+    // nodes in left and right subtrees in the original tree
     
+    int helper(Node* root){
+        if(!root){
+            return 0;
+        }
+        
+        int left = helper(root->left);
+        int right = helper(root->right);
+        int temp = root->data;
+        root->data = left+right;
+        return left+right+temp;
+    }
+    void toSumTree(Node *node)
+    {
+        // Your code here
+        helper(node);
+        return;
+    }
 };
 
-void sumReplacement(node* root)
-{
-    if(root == NULL){
-        return;
-    }
-    sumReplacement(root->left);
-    sumReplacement(root->right);
-    if(root->left){
-        root->data = root->data + root->left->data;
-    }
-    if(root->right){
-        root->data = root->data + root->right->data;
-    }
-
-    return;
-}
-
-void preorder(node* root){
-    if(root == NULL){
-        return;
-    }
-    cout << root->data << endl;
-    preorder(root->left);
-    preorder(root->right);
-}
-
-int countNodes(node* root){
-    if(root == NULL){
-        return 0;
-    }
-    return 1 + countNodes(root->left) + countNodes(root->right);
-}
-
-int main()
-{
-    node* root = new node(3);
-    root->left = new node(4);
-    root->right = new node(5);
-    root->left->left = new node(6);
-    root->left->right = new node(7);
-    root->right->right = new node(9);
-    sumReplacement(root);
-    preorder(root);
-    cout << "node count " << countNodes(root) << endl;
-    return 0;
-}
 
 
