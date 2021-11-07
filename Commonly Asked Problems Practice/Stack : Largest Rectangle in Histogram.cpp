a15c30ae3cfac5050b8ae4115adf071ba54a82cc
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-11-07 11:20:05 +0530

    Update Stack : Largest Rectangle in Histogram.cpp

--- Commonly Asked Problems Practice/Stack : Largest Rectangle in Histogram.cpp
+++ Commonly Asked Problems Practice/Stack : Largest Rectangle in Histogram.cpp
@@ -1,5 +1,6 @@
 /*
-Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
+return the area of the largest rectangle in the histogram.
 
  
 
